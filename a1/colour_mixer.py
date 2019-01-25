1f0cc4d8a18969ff99692b9d2ad72a9c1fc46f09
JoshuaShin <joshua_shin@hotmail.com>
2019-01-24 23:17:33 -0800

    Implement colour_mixer.py
    Function color mix prints secondary color from two primary color inputs.

---
+++ a1/colour_mixer.py
@@ -0,0 +1,50 @@
+"""
+colour_mixer.py
+
+Print secondary color from two primary color inputs.
+"""
+
+# Joshua Shin
+# A01056181
+# Jan 24 2019
+
+
+def color_mixer():
+    """
+    Print secondary color from two primary color inputs.
+    """
+
+    color = ["", ""]
+    color[0] = input("First primary color: ").strip().lower()
+    if not(color[0] == "red" or color[0] == "yellow" or color[0] == "blue"):
+        print("Please input a primary color!")
+        color_mixer()
+
+    color[1] = input("Second primary color: ").strip().lower()
+    if not(color[1] == "red" or color[1] == "yellow" or color[1] == "blue"):
+        print("Please input a primary color!")
+        color_mixer()
+
+    if color[0] == color[1]:
+        print("First and second colors must be different!")
+        color_mixer()
+
+    if "red" in color and "blue" in color:
+        print("Secondary color: purple")
+
+    elif "red" in color and "yellow" in color:
+        print("Secondary color: orange")
+
+    elif "yellow" in color and "blue" in color:
+        print("Secondary color: green")
+
+
+def main():
+    """
+    Drive the program.
+    """
+    color_mixer()
+
+
+if __name__ == "__main__":
+    main()
