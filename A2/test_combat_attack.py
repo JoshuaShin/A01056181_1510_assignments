f8aa435656e3e4f1dbd5480ede98412d549c5540
JoshuaShin <joshua_shin@hotmail.com>
2019-02-12 19:27:17 -0800

    Implement test_combat_attack unit test

---
+++ A2/test_combat_attack.py
@@ -0,0 +1,28 @@
+import io
+from unittest.mock import patch
+from unittest import TestCase
+from dungeonsanddragons import combat_attack
+
+attacker = {'Name': 'A', 'Class': 'barbarian', 'HP': 10, 'Strength': 10, 'Dexterity': 10,
+            'Constitution': 10, 'Intelligence': 10, 'Wisdom': 10, 'Charisma': 10, 'XP': 0, 'Inventory': []}
+defender = {'Name': 'B', 'Class': 'barbarian', 'HP': 10, 'Strength': 10, 'Dexterity': 10,
+            'Constitution': 10, 'Intelligence': 10, 'Wisdom': 10, 'Charisma': 10, 'XP': 0, 'Inventory': []}
+
+
+class TestCombatAttack(TestCase):
+    @patch('dungeonsanddragons.roll_die', side_effect=[11, 0])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_attack_success(self, mock_stdout, input):
+        combat_attack(attacker, defender)
+        self.assertTrue("hits" in mock_stdout.getvalue())
+
+    @patch('dungeonsanddragons.roll_die', side_effect=[10, 0])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_attack_fail(self, mock_stdout, input):
+        combat_attack(attacker, defender)
+        self.assertTrue("misses" in mock_stdout.getvalue())
+
+    @patch('dungeonsanddragons.roll_die', side_effect=[11, 1])
+    def test_combat_attack_damage(self, input):
+        combat_attack(attacker, defender)
+        self.assertEqual(defender["HP"], 9)
--- A2/test_combat_initiative.py
+++ A2/test_combat_initiative.py
@@ -3,23 +3,23 @@ from unittest.mock import patch
 from unittest import TestCase
 from dungeonsanddragons import combat_initiative
 
-character_1 = {'Name': 'A', 'Class': 'barbarian', 'HP': 12, 'Strength': 9, 'Dexterity': 7,
-               'Constitution': 13, 'Intelligence': 4, 'Wisdom': 15, 'Charisma': 11, 'XP': 0, 'Inventory': []}
-character_2 = {'Name': 'B', 'Class': 'barbarian', 'HP': 12, 'Strength': 9, 'Dexterity': 7,
-               'Constitution': 13, 'Intelligence': 4, 'Wisdom': 15, 'Charisma': 11, 'XP': 0, 'Inventory': []}
+opponent_1 = {'Name': 'A', 'Class': 'barbarian', 'HP': 10, 'Strength': 10, 'Dexterity': 10,
+              'Constitution': 10, 'Intelligence': 10, 'Wisdom': 10, 'Charisma': 10, 'XP': 0, 'Inventory': []}
+opponent_2 = {'Name': 'A', 'Class': 'barbarian', 'HP': 10, 'Strength': 10, 'Dexterity': 10,
+              'Constitution': 10, 'Intelligence': 10, 'Wisdom': 10, 'Charisma': 10, 'XP': 0, 'Inventory': []}
 
 
 class TestCombatInitiative(TestCase):
     @patch('dungeonsanddragons.roll_die', side_effect=[2, 1])
     def test_combat_initiative_opponent_1_wins(self, input):
-        self.assertEqual(combat_initiative(character_1, character_2), (character_1, character_2))
+        self.assertEqual(combat_initiative(opponent_1, opponent_2), (opponent_1, opponent_2))
 
     @patch('dungeonsanddragons.roll_die', side_effect=[1, 2])
     def test_combat_initiative_opponent_2_wins(self, input):
-        self.assertEqual(combat_initiative(character_1, character_2), (character_2, character_1))
+        self.assertEqual(combat_initiative(opponent_1, opponent_2), (opponent_2, opponent_1))
 
     @patch('dungeonsanddragons.roll_die', side_effect=[1, 1, 1, 2])
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_combat_initiative_tie(self, mock_stdout, input):
-        combat_initiative(character_1, character_2)
+        combat_initiative(opponent_1, opponent_2)
         self.assertTrue("Tied. Re-roll...\n" in mock_stdout.getvalue())
