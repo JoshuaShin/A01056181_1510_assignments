2441f9f879917a7c08db33376e51d4ea6c92d7b4
JoshuaShin <joshua_shin@hotmail.com>
2019-03-01 15:44:23 -0800

    Implement roll_die

--- A3/sud.py
+++ A3/sud.py
@@ -20,5 +20,33 @@ def main():
     pass
 
 
+def roll_die(number_of_rolls, number_of_sides):
+    """
+    Simulate rolling a die of the specified size the specified number of times.
+
+    PARAM positive integer
+    PARAM positive integer
+    PRE-CONDITION integer must be positive
+    PRE-CONDITION integer must be positive
+    RETURN the sum of the die of the specified size rolled the specified number of times
+
+    >>> random.seed(0)
+    >>> roll_die(0, 0)
+    0
+    >>> roll_die(1, 1)
+    1
+    >>> roll_die(6, 1)
+    6
+    >>> roll_die(1, 6)
+    4
+    >>> random.seed()
+    """
+
+    if number_of_rolls < 1 or number_of_sides < 1:
+        return 0
+    else:
+        return random.randrange(1, number_of_sides + 1, 1) + roll_die(number_of_rolls - 1, number_of_sides)
+
+
 if __name__ == "__main__":
     main()
