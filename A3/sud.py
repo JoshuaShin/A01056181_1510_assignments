92b83dcc9a3847f804d395866f9fc1b55f34ac88
JoshuaShin <joshua_shin@hotmail.com>
2019-03-01 15:40:34 -0800

    Create sud.py

---
+++ A3/sud.py
@@ -0,0 +1,24 @@
+"""
+sud.py
+
+Single User Dungeon game.
+Type commands to interact with the environment.
+Commands: north, east, south, weest, fight, flee.
+"""
+
+
+# Joshua Shin
+# A01056181
+# Mar 1st 2019
+
+
+import random
+import doctest
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
