cc38d8b55cad5fb023f33317cc5bbd28f1070c7d
JoshuaShin <joshua_shin@hotmail.com>
2019-03-10 15:43:56 -0700

    Implement test_combat_flee unit test

--- A3/test_combat_flee.py
+++ A3/test_combat_flee.py
@@ -1,6 +1,26 @@
+import io
+from unittest.mock import patch
 from unittest import TestCase
+from combat import combat_flee
 
 
 class TestCombatFlee(TestCase):
-    def test_combat_flee(self):
-        self.fail()
+    @patch('builtins.input', side_effect=['y'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_flee_print(self, mock_stdout, mock_input):
+        combat_flee()
+        self.assertTrue("ENEMY ENCOUNTER" in mock_stdout.getvalue())
+
+    @patch('builtins.input', side_effect=['y'])
+    def test_combat_flee_yes(self, mock_input):
+        self.assertEqual(False, combat_flee())
+
+    @patch('builtins.input', side_effect=['n'])
+    def test_combat_flee_no(self, mock_input):
+        self.assertEqual(True, combat_flee())
+
+    @patch('builtins.input', side_effect=['invalid input', 'y'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_flee_invalid_input(self, mock_stdout, mock_input):
+        combat_flee()
+        self.assertTrue("INVALID COMMAND" in mock_stdout.getvalue())
