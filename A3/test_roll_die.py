dca69cc54dfbbc608df11f2f70cc4bcfb932eb50
JoshuaShin <joshua_shin@hotmail.com>
2019-03-09 23:40:16 -0800

    Implement test_roll_die unit test

---
+++ A3/test_roll_die.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from combat import roll_die
+import random
+
+
+class TestRollDie(TestCase):
+    def test_roll_die_integer(self):
+        self.assertEqual(type(roll_die(0, 10)), int)
+
+    def test_roll_die_no_sides(self):
+        self.assertEqual(roll_die(10, 0), 0)
+
+    def test_roll_die_no_rolls(self):
+        self.assertEqual(roll_die(0, 10), 0)
+
+    def test_roll_die_range(self):
+        for i in range(0, 1000):
+            random_sides = random.randint(1, 20)
+            random_rolls = random.randint(1, 4)
+            self.assertTrue(random_rolls <= roll_die(random_rolls, random_sides) <= random_rolls * random_sides)
+
