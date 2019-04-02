e0fa251589245039e5716d6f6c11253b465e5f5f
JoshuaShin <joshua_shin@hotmail.com>
2019-04-02 16:37:16 -0700

    Implement test_string_to_good_standing function

---
+++ A4/test_string_to_good_standing.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import crud
+
+
+class TestStringToGoodStanding(TestCase):
+    def test_string_to_good_standing_true(self):
+        status = crud.string_to_good_standing("true")
+        self.assertEqual(True, status)
+
+    def test_string_to_good_standing_false(self):
+        status = crud.string_to_good_standing("false")
+        self.assertEqual(False, status)
+
+    def test_string_to_good_standing_invalid(self):
+        with self.assertRaises(ValueError):
+            crud.string_to_good_standing("invalid input")
+
+
