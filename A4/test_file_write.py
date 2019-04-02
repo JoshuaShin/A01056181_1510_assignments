aead24ee7bd4d8de81d1d7a48d7bf21441c40b4b
JoshuaShin <joshua_shin@hotmail.com>
2019-04-01 22:59:03 -0700

    Implement test_file_write unit test

---
+++ A4/test_file_write.py
@@ -0,0 +1,19 @@
+import io
+from unittest.mock import patch
+from unittest import TestCase
+import crud
+import student
+
+
+class TestFileWrite(TestCase):
+    def test_file_write_blank(self):
+        crud.file_write([])
+        self.assertEqual(0, len(crud.file_read()))
+
+    def test_file_write(self):
+        student_1 = student.Student("test", "test", "t11111111", "True", [])
+        student_2 = student.Student("test", "test", "t22222222", "True", [])
+
+        crud.file_write([student_1, student_2])
+        self.assertTrue(crud.is_student_number_duplicate("t11111111") and crud.is_student_number_duplicate("t11111111"))
+        crud.file_write([])
