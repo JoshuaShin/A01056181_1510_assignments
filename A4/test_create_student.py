6ab6fc45c59b5b91c021c79f60e65c9a0833ca9a
JoshuaShin <joshua_shin@hotmail.com>
2019-04-02 00:04:37 -0700

    Modify test_create_student unit test

--- A4/test_create_student.py
+++ A4/test_create_student.py
@@ -1,9 +1,22 @@
-import io
 from unittest.mock import patch
 from unittest import TestCase
 import crud
 
 
 class TestCreateStudent(TestCase):
-    def test_create_student(self):
-        self.fail()
+    @patch('builtins.input', side_effect=["test", "test", "t12345678", "True", ""])
+    def test_create_student_successfully(self, mock_input):
+        student = crud.create_student()
+        self.assertEqual("Test Test T12345678 True", str(student).strip())
+
+    @patch('builtins.input', side_effect=["test", "test", "t12345678", "True", "", "test", "test", "t12345678"])
+    def test_create_student_duplicate_student_number(self, mock_input):
+        crud.add_student()
+        with self.assertRaises(ValueError):
+            crud.create_student()
+        crud.file_write([])
+
+    @patch('builtins.input', side_effect=["test", "test", "t12345678", "True", "not number"])
+    def test_create_student_invalid_grade(self, mock_input):
+        with self.assertRaises(ValueError):
+            crud.create_student()
