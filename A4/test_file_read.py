3e8cee767390aa2585117dd3610cede06c65bbd6
JoshuaShin <joshua_shin@hotmail.com>
2019-04-01 23:03:59 -0700

    Implement test_file_read unit test

--- A4/test_file_read.py
+++ A4/test_file_read.py
@@ -1,17 +1,21 @@
+import io
+from unittest.mock import patch
 from unittest import TestCase
 import crud
 import student
 
 
-class TestFileWrite(TestCase):
-    def test_file_write_blank(self):
+class TestFileRead(TestCase):
+    def test_file_read_blank(self):
         crud.file_write([])
         self.assertEqual(0, len(crud.file_read()))
 
-    def test_file_write(self):
+    def test_file_read(self):
         student_1 = student.Student("test", "test", "t11111111", "True", [])
         student_2 = student.Student("test", "test", "t22222222", "True", [])
 
         crud.file_write([student_1, student_2])
-        self.assertTrue(crud.is_student_number_duplicate("t11111111") and crud.is_student_number_duplicate("t11111111"))
+        students = crud.file_read()
+        self.assertTrue(student_1.get_student_number() in students[0].get_student_number()
+                        and student_2.get_student_number() in students[1].get_student_number())
         crud.file_write([])
